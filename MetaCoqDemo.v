d73a8d228fd8d673a5636d65fcd1f4d4afd9cf70
Johannes Hostert <s8johost@stud.uni-saarland.de>
2020-11-10 21:08:13 +0100

    Initial commit

---
+++ MetaCoqDemo.v
@@ -0,0 +1,441 @@
+(*
+                             ...          ...
+                              |            /\
+                    parsing   |            | printing
+                              |            |
+                              V            |
+                       intf/Constrexpr.constr_expr
+                              |            /\
+                 constrintern |            | constrextern
+                  (in interp) |            | (in interp)
+                globalization |            |
+                              V            |
+                           Glob_term.glob_constr
+                              |            /\
+                    pretyping |            | detyping
+                              |            | (in pretyping)
+                              V            |
+                               Term.constr
+                                 |     /\
+                   safe_typing   |      |
+                   (validation   |      |
+                      by kernel) |______|
+
+  coq/kernel/constr.ml
+
+*)
+
+
+From MetaCoq.Template Require Import utils All.
+Require Import List String.
+Import MonadNotation.
+Import ListNotations.
+Open Scope string_scope.
+
+(** * The term type  *)
+
+Print Ast.term.
+
+(** * Quoting and unquoting  *)
+
+MetaCoq Test Quote (fun x : nat => x).
+
+MetaCoq Test Quote (fun (f : nat -> nat) (x : nat) => f x).
+
+MetaCoq Test Quote (let x := 2 in x).
+
+MetaCoq Test Quote (let x := 2 in
+            match x with
+              | 0 => 0
+              | S n => n
+            end).
+
+MetaCoq Test Quote 0.
+MetaCoq Test Unquote (Ast.tConstruct (mkInd (MPfile ["Datatypes"; "Init"; "Coq"], "nat") 0) 1 []).
+
+(** Fixpoints **)
+Fixpoint add (a b : nat) : nat :=
+  match a with
+    | 0 => b
+    | S a => S (add a b)
+  end.
+Eval vm_compute in add.
+
+MetaCoq Test Quote add.
+
+Fixpoint add' (a b : nat) : nat :=
+  match b with
+    | 0 => a
+    | S b => S (add' a b)
+  end.
+
+Fixpoint even (a : nat) : bool :=
+  match a with
+    | 0 => true
+    | S a => odd a
+  end
+with odd (a : nat) : bool :=
+  match a with
+    | 0 => false
+    | S a => even a
+  end.
+
+MetaCoq Quote Definition add_syntax := Eval compute in add.
+Print add_syntax.
+MetaCoq Quote Recursively Definition add_prog := add.
+Print add_prog.
+
+MetaCoq Quote Definition eo_syntax := Eval compute in even.
+
+MetaCoq Quote Definition add'_syntax := Eval compute in add'.
+
+MetaCoq Unquote Definition t := add_syntax.
+Print t.
+
+(** * Example function: Unfolding constants *)
+
+Fixpoint unfold_consts (Σ : global_env) (t : term) :=
+  match t with
+  | tRel i => tRel i
+  | tEvar ev args => tEvar ev (List.map (unfold_consts Σ ) args)
+  | tLambda na T M => tLambda na (unfold_consts Σ  T) (unfold_consts Σ  M)
+  | tApp u v => tApp (unfold_consts Σ  u) (List.map (unfold_consts Σ ) v)
+  | tProd na A B => tProd na (unfold_consts Σ  A) (unfold_consts Σ  B)
+  | tCast C kind t => tCast (unfold_consts Σ  C) kind (unfold_consts Σ  t)
+  | tLetIn na b t b' => tLetIn na (unfold_consts Σ  b) (unfold_consts Σ  t) (unfold_consts Σ  b')
+  | tCase ind p C brs =>
+    let brs' := List.map (on_snd (unfold_consts Σ )) brs in
+    tCase ind (unfold_consts Σ  p) (unfold_consts Σ  C) brs'
+  | tProj p C => tProj p (unfold_consts Σ  C)
+  | tFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts Σ ) (unfold_consts Σ )) mfix in
+    tFix mfix' idx
+  | tCoFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts Σ ) (unfold_consts Σ )) mfix in
+    tCoFix mfix' idx
+  | tConst name u => match lookup_env Σ name with
+                    | Some (ConstantDecl (Build_constant_body _ (Some body) u)) => body
+                    | _ => tConst name u
+                    end
+  | x => x
+  end.
+
+Fixpoint unfold_consts_env (Σ : global_env) :=
+  match Σ with
+    (na , ConstantDecl cnst) :: Σ =>
+    let Σ' := unfold_consts_env Σ in
+    ((na, ConstantDecl (map_constant_body (unfold_consts Σ') cnst))) :: Σ'
+  | c :: Σ => c :: unfold_consts_env Σ
+  | [] => []
+  end.
+
+Definition two := 2.
+Lemma three : nat. exact (1 + two). Qed.
+
+MetaCoq Quote Recursively Definition bla := three.
+
+Definition Σ := fst bla.
+Compute Σ.
+Print add.
+MetaCoq Test Unquote (tConstruct
+                                 {|
+                                 inductive_mind := (MPfile ["Datatypes"; "Init"; "Coq"], "nat");
+                                 inductive_ind := 0 |} 1 []).
+Definition ter := snd bla.
+Compute ter.
+
+Definition ter' :=let Σ' := unfold_consts_env Σ in
+         let ter' := unfold_consts Σ' ter in
+         ter'.
+Compute ter'.
+
+MetaCoq Unquote Definition def := ter'.
+Print def.
+
+Definition unfoldAllConsts {A} (a : A) :=
+  pair <- tmQuoteRec a ;;
+  let Σ' := fst pair in
+  let t' := snd pair in
+  let Σ := unfold_consts_env Σ' in
+  let t := unfold_consts Σ t' in
+  tmUnquoteTyped A t.
+
+Definition unfoldAllConstsDef {A} (na : string) (a : A) :=
+  res <- unfoldAllConsts a ;;
+  tmDefinition na res.
+
+MetaCoq Run (res <- unfoldAllConsts three ;; tmPrint res).
+
+MetaCoq Run (unfoldAllConstsDef "three_transp" three).
+Print three_transp.
+
+(** ** Unfolding and reducing constants *)
+
+From MetaCoq Require Import Checker.All.
+Require Import String List.
+Open Scope string_scope.
+
+Definition FUEL := 100.
+
+Definition reduce Σ t := match reduce_opt RedFlags.default Σ [] FUEL t with Some res => res | _ => tVar "noresult" end.
+
+Fixpoint unfold_consts_red (Σ : global_env) (t : term) :=
+  match t with
+  | tRel i => tRel i
+  | tEvar ev args => tEvar ev (List.map (unfold_consts_red Σ ) args)
+  | tLambda na T M => tLambda na (unfold_consts_red Σ  T) (unfold_consts_red Σ  M)
+  | tApp u v => tApp (unfold_consts_red Σ  u) (List.map (unfold_consts_red Σ ) v)
+  | tProd na A B => tProd na (unfold_consts_red Σ  A) (unfold_consts_red Σ  B)
+  | tCast C kind t => tCast (unfold_consts_red Σ  C) kind (unfold_consts_red Σ  t)
+  | tLetIn na b t b' => tLetIn na (unfold_consts_red Σ  b) (unfold_consts_red Σ  t) (unfold_consts_red Σ  b')
+  | tCase ind p C brs =>
+    let brs' := List.map (on_snd (unfold_consts_red Σ )) brs in
+    tCase ind (unfold_consts_red Σ  p) (unfold_consts_red Σ  C) brs'
+  | tProj p C => tProj p (unfold_consts_red Σ  C)
+  | tFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts_red Σ ) (unfold_consts_red Σ )) mfix in
+    tFix mfix' idx
+  | tCoFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts_red Σ ) (unfold_consts_red Σ )) mfix in
+    tCoFix mfix' idx
+  | tConst name u => match lookup_env Σ name with
+                    | Some (ConstantDecl (Build_constant_body _ (Some body) u)) => reduce Σ body
+                    | _ => tConst name u
+                    end
+  | x => x
+  end.
+
+Fixpoint unfold_consts_red_env (Σ : global_env) :=
+  match Σ with
+    (na , ConstantDecl cnst) :: Σ =>
+    let Σ' := unfold_consts_red_env Σ in
+    ((na, ConstantDecl (map_constant_body (unfold_consts_red Σ') cnst))) :: Σ'
+  | c :: Σ => c :: unfold_consts_red_env Σ
+  | [] => []
+  end.
+
+Definition makeTransparent_red {A} (a : A) :=
+  mlet (Σ', t') <- tmQuoteRec a ;;
+  let Σ := unfold_consts_red_env Σ' in
+  let t := unfold_consts_red Σ t' in
+  tmUnquoteTyped A t.
+
+Definition makeTransparentDef_red {A} (na : string) (a : A) :=
+  res <- makeTransparent_red a ;;
+  tmDefinition na res.
+MetaCoq Run (res <- makeTransparent_red three ;; tmPrint res).
+MetaCoq Run (makeTransparentDef_red "three_transp_red" (1 + three)).
+Print three_transp_red.
+
+(** ** Unfolding all constants, reducing propositional constants *)
+
+Existing Instance config.default_checker_flags.
+
+Fixpoint unfold_consts_red_prop (Σ : global_env) (t : term) :=
+  match t with
+  | tRel i => tRel i
+  | tEvar ev args => tEvar ev (List.map (unfold_consts_red_prop Σ ) args)
+  | tLambda na T M => tLambda na (unfold_consts_red_prop Σ  T) (unfold_consts_red_prop Σ  M)
+  | tApp u v => tApp (unfold_consts_red_prop Σ  u) (List.map (unfold_consts_red_prop Σ ) v)
+  | tProd na A B => tProd na (unfold_consts_red_prop Σ  A) (unfold_consts_red_prop Σ  B)
+  | tCast C kind t => tCast (unfold_consts_red_prop Σ  C) kind (unfold_consts_red_prop Σ  t)
+  | tLetIn na b t b' => tLetIn na (unfold_consts_red_prop Σ  b) (unfold_consts_red_prop Σ  t) (unfold_consts_red_prop Σ  b')
+  | tCase ind p C brs =>
+    let brs' := List.map (on_snd (unfold_consts_red_prop Σ )) brs in
+    tCase ind (unfold_consts_red_prop Σ  p) (unfold_consts_red_prop Σ  C) brs'
+  | tProj p C => tProj p (unfold_consts_red_prop Σ  C)
+  | tFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts_red_prop Σ ) (unfold_consts_red_prop Σ )) mfix in
+    tFix mfix' idx
+  | tCoFix mfix idx =>
+    let mfix' := List.map (map_def (unfold_consts_red_prop Σ ) (unfold_consts_red_prop Σ )) mfix in
+    tCoFix mfix' idx
+  | tConst name u => match lookup_env Σ name with
+                    | Some (ConstantDecl (Build_constant_body _ (Some body) u)) =>
+                      match infer (F := FUEL) Σ init_graph [] body with
+                      | Checked (tSort s) => if Universe.is_prop s then reduce Σ body else body
+                      | _ => body
+                      end
+                    | _ => tConst name u
+                    end
+  | x => x
+  end.
+
+Fixpoint unfold_consts_red_prop_env (Σ : global_env) :=
+  match Σ with
+    (na , ConstantDecl cnst) :: Σ =>
+    let Σ' := unfold_consts_red_prop_env Σ in
+    ((na, ConstantDecl (map_constant_body (unfold_consts_red_prop Σ') cnst))) :: Σ'
+  | c :: Σ => c :: unfold_consts_red_prop_env Σ
+  | [] => []
+  end.
+
+Definition makeTransparent_red_prop {A} (a : A) :=
+  mlet (Σ', t') <- tmQuoteRec a ;;
+  let Σ := unfold_consts_red_prop_env Σ' in
+  let t := unfold_consts_red_prop Σ t' in
+  tmUnquoteTyped A t.
+
+Definition makeTransparentDef_red_prop {A} (na : string) (a : A) :=
+  res <- makeTransparent_red_prop a ;;
+  tmDefinition na res.
+
+MetaCoq Run (res <- makeTransparent_red_prop three ;; tmPrint res).
+
+Definition cst : Prop. Proof. exact ((fun x : Prop => x) True). Qed.
+
+Definition four : nat.
+  exact (fst (1 + three, cst)).
+Qed.
+MetaCoq Run (makeTransparent_red_prop four >>= tmPrint).
+
+(** * The Template Monad  *)
+
+Print TemplateMonad.
+
+Local Notation Nat_module := (MPfile ["Datatypes"; "Init"; "Coq"], "nat").
+Notation inat :=
+  {| inductive_mind := Nat_module; inductive_ind := 0 |}.
+
+MetaCoq Run (t <- tmQuote (3 + 3) ;; tmPrint t).
+
+MetaCoq Run (t <- tmQuoteRec add ;; tmPrint t ).
+
+Locate add.
+MetaCoq Run (t <- tmLocate "add" ;; tmPrint t).
+
+Definition printInductive (q : qualid): TemplateMonad unit :=
+  kn <- tmLocate1 q ;;
+  match kn with
+  | IndRef ind => (tmQuoteInductive ind.(inductive_mind)) >>= tmPrint
+  | _ => tmFail ("[" ++ q ++ "] is not an inductive")
+  end.
+
+MetaCoq Run (printInductive "Coq.Init.Datatypes.nat").
+MetaCoq Run (printInductive "nat").
+Fail MetaCoq Run (printInductive "natt").
+Fail MetaCoq Run (printInductive "S").
+
+MetaCoq Run (tmDefinition "foo" nat >>= tmPrint).
+Compute foo.
+
+MetaCoq Run (t <- tmDefinition "foo5'" 12 ;;
+                     tmDefinition "foo6'" t).
+Print foo5'.
+Print foo6'.
+
+MetaCoq Run (tmLemma "foo51" nat ;;
+                     tmLemma "foo61" bool).
+Next Obligation.
+  exact 3.
+Defined.
+Next Obligation.
+  exact true.
+Qed.
+Compute foo51.
+Compute foo61.
+
+MetaCoq Run (tmAxiom "fal" False).
+
+(** * PCUIC  *)
+
+Print term.
+
+Require Import MetaCoq.PCUIC.PCUICAst MetaCoq.PCUIC.PCUICTyping. 
+
+Print term.
+Print typing.
+
+Require Import MetaCoq.PCUIC.PCUICSR MetaCoq.PCUIC.PCUICConfluence MetaCoq.PCUIC.PCUICValidity.
+
+(* Ex 0 (challenge):
+   Write down the right correctness lemma, and then try to prove it.
+   Lemma unfold_correct : *)
+(*
+Σ ;;; Γ |- t : T ->
+Σ ;;; Γ |- t ->* unfold_consts (unfold_consts_env Σ) t : T. *)
+
+
+(** * Running in tactics  *)
+
+Goal forall n, n > 0 -> exists m, m < 0.
+Proof.
+  Time match goal with [ |- ?G ] =>
+  let k y := idtac y in
+  run_template_program (tmQuote G) k
+  end.
+Abort.
+
+(** * Extraction to OCaml  *)
+
+Extraction mult.
+
+Lemma test : forall x, { y | y > 2 * x}.
+Proof.
+  intros x. exists (2 * x + 1). lia.
+Defined.
+
+Extraction test.
+
+(** * Erasure  *)
+
+Require MetaCoq.Erasure.EAst MetaCoq.Erasure.ErasureFunction.
+
+Print EAst.term.
+Print ErasureFunction.erase.
+
+(** * Type-checking and erasure  *)
+
+From MetaCoq.Template Require Import Loader.
+From MetaCoq.Erasure Require Import Loader.
+From MetaCoq.SafeChecker Require Import Loader.
+
+MetaCoq SafeCheck nat.
+MetaCoq SafeCheck (forall X : Prop, Prop).
+MetaCoq SafeCheck 0.
+
+MetaCoq Erase nat.
+
+MetaCoq Erase I.
+MetaCoq Erase true.
+
+MetaCoq Erase (@exist nat (fun x => x = 0) 0 (eq_refl) : {x : nat | x = 0}).
+
+MetaCoq Erase (ltac:(let x := eval unfold test in test in exact x)).
+
+(*
+  Ex 1: MetaCoq. Write a function which normalizes all propositional subterms (have type Prop). Then write a function which normalizes all subterms which are proofs.
+*)
+
+(*
+  Ex 2: Reflection using Metacoq. Write a reflection function like on day 2 for the simplification of propositional proof goals, this time with MetaCoq.
+*)
+
+(*
+ Ex 3: Extraction.
+
+  1. Define inductively a relation Fib which specifies the fibonnaci function (fib 0=0 fib 1=1 fib (n+2)=fib n+fib (n+1)) as a binary relation.
+  2. Prove the following principle by one simple induction: ∀ P:nat→Type, P 0 → P 1 → (∀ n, P n → P (S n) → P (S (S n))) → ∀ n,P n
+  3. Use this principle in order to prove ∀ n, {m|Fib n m}
+  4. Look at the extracted program, what is its complexity ?
+  5. Propose another proof of the induction principle using the Fixpoint construction, compare the programs obtained. 
+
+*)
+
+(*
+ Ex (challenge): Write down a correctness statement for the reflection function
+
+ Something along the lines of
+
+  MetaCoq Quote Definition eval_syntax := eval.
+  Definition eval_ := trans eval.
+
+  MetaCoq Quote Definition eval_syntax := eval.
+  Definition eval_ := trans eval.
+
+  Σ ;;; Γ |- p : tSort u ->
+  is_prop u ->
+  reflect p = Some r ->
+  exists prf, Σ ;;; Γ |- prf : mkApps eq_ [tApp eval_ r ; p]
+
+ *)
---
+++ Reflection.v
@@ -0,0 +1,356 @@
+From MetaCoq.Template Require Import utils All Pretty.
+Print term.
+Locate term.
+Require Import List String.
+Import MonadNotation.
+
+Require Import FOL.
+Require Import Deduction.
+Require Import Tarski.
+Require Import VectorTech.
+Require Import Lia.
+
+
+(* I follow the treatment of Peter Smith in "Introduction to Gödel's Theorems"
+ (page 37) *)
+
+
+(* Define the non-logical symbols used in the language of PA *)
+
+Inductive PA_funcs : Type :=
+  Zero : PA_funcs
+| Succ : PA_funcs
+| Plus : PA_funcs
+| Mult : PA_funcs.
+
+Definition PA_funcs_ar (f : PA_funcs ) :=
+match f with
+ | Zero => 0
+ | Succ => 1
+ | Plus => 2
+ | Mult => 2
+ end.
+
+Inductive PA_preds : Type :=
+  Eq : PA_preds.
+
+Definition PA_preds_ar (P : PA_preds) :=
+match P with
+ | Eq => 2
+end.
+
+
+Instance PA_funcs_signature : funcs_signature :=
+{| syms := PA_funcs ; ar_syms := PA_funcs_ar |}.
+
+Instance PA_preds_signature : preds_signature :=
+{| preds := PA_preds ; ar_preds := PA_preds_ar |}.
+
+ 
+Arguments Vector.cons {_} _ {_} _, _ _ _ _.
+
+Definition zero := func Zero (Vector.nil term).
+Notation "'σ' x" := (func Succ (Vector.cons x (Vector.nil term))) (at level 37).
+Notation "x '⊕' y" := (func Plus (Vector.cons x (Vector.cons y (Vector.nil term))) ) (at level 39).
+Notation "x '⊗' y" := (func Mult (Vector.cons x (Vector.cons y (Vector.nil term))) ) (at level 38).
+Notation "x '==' y" := (atom Eq (Vector.cons term x 1 (Vector.cons term y 0 (Vector.nil term))) ) (at level 40).
+
+
+
+Fixpoint num n :=
+  match n with
+    O => zero
+  | S x => σ (num x)
+  end.
+
+                      
+
+
+(* formulate axioms of PA (see page 92) *)
+
+Definition ax_zero_succ := ∀    zero == σ var 0 --> fal.
+Definition ax_succ_inj :=  ∀ ∀  σ $1 == σ $0 --> $1 == $0.
+Definition ax_add_zero :=  ∀    zero ⊕ $0 == $0.
+Definition ax_add_rec :=   ∀ ∀  (σ $0) ⊕ $1 == σ ($0 ⊕ $1).
+Definition ax_mult_zero := ∀    zero ⊗ $0 == zero.
+Definition ax_mult_rec :=  ∀ ∀  σ $1 ⊗ $0 == $0 ⊕ $1 ⊗ $0.
+
+
+Definition PA_induction (phi : form) :=
+  phi[zero..] --> (∀ phi --> phi[σ $0 .: S >> var]) --> ∀ phi.
+Definition phi := $0 == $1.
+Compute (phi[zero..]).
+Compute (phi[zero .: S >> var]).
+
+(* substitutes t for the variable $0 and leaves all other variables unchanged *)
+Definition var0_subst (t : term) : nat -> term :=
+  fun n => match n with 0 => t | S n => var (S n) end.
+
+
+(* var0_subst can be expressed with scons and funcomp *)
+Lemma var0_subst_spec t n :
+  var0_subst t n = (t .: S >> var) n.
+Proof.
+  now destruct n as [].
+Qed.
+
+
+
+
+                                              
+
+(*** Working in models of PA ***)
+                                              
+Section Models.                                              
+
+  Variable D : Type.
+  Context {I : interp D}.
+
+  Definition Equality := forall v, @i_P _ _ D I Eq v <-> Vector.hd v = Vector.hd (Vector.tl v). 
+  Hypothesis equality : forall v, @i_P _ _ D I Eq v <-> Vector.hd v = Vector.hd (Vector.tl v).
+
+  Notation "'iO'" := (i_f (f:=Zero) (Vector.nil D)).
+  Notation "'iσ' d" := (i_f (f:=Succ) (Vector.cons d (Vector.nil D))) (at level 37).
+  Notation "x 'i⊕' y" := (i_f (f:=Plus) (Vector.cons x (Vector.cons y (Vector.nil D)))) (at level 39).
+  Notation "x 'i⊗' y" := (i_f (f:=Mult) (Vector.cons x (Vector.cons y (Vector.nil D)))) (at level 38).
+  Notation "x 'i=' y" := (i_P (P:=Eq)   (Vector.cons x (Vector.cons y (Vector.nil D)))) (at level 40).
+  Definition iμ k := iter (fun x => iσ x) k iO.
+  Fixpoint naryProp (n:nat) : Type := match n with 0 => Prop | S nn => D -> naryProp nn end.
+  Fixpoint representsP {n:nat} phi rho : (forall (P:naryProp n), Prop) := match n return (forall (P:naryProp n), Prop) with
+       0  => (fun (P:Prop) => P <-> rho ⊨ phi)
+    | S n => (fun (P:D -> naryProp n) => forall d, @representsP n phi (d.:rho) (P d)) end.
+  Definition representableP (n:nat) (P : naryProp n) := exists phi rho, representsP phi rho P.
+  Definition representsF (d:D) trm rho := eval rho trm = d.
+  Definition representableF (d:D) := exists trm rho, representsF d trm rho.
+
+
+Notation quoteO := (tApp (tConst (MPfile (["Tarski"; "Arith"]), "i_f") nil)
+   ([tConst (MPfile (["Reflection"; "Arith"]), "PA_funcs_signature") (nil);
+   tConst (MPfile (["Reflection"; "Arith"]), "PA_preds_signature") nil;
+   tVar "D"; tVar "I";
+   tConstruct
+     {|
+     inductive_mind := (MPfile (["Reflection"; "Arith"]), "PA_funcs");
+     inductive_ind := 0 |} 0 nil;
+   tApp
+     (tConstruct
+        {|
+        inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t");
+        inductive_ind := 0 |} 0 nil) ([tVar "D"])])).
+Notation quoteS x := 
+(tApp (tConst (MPfile (["Tarski"; "Arith"]), "i_f") nil)
+   ([tConst (MPfile (["Reflection"; "Arith"]), "PA_funcs_signature") nil;
+   tConst (MPfile (["Reflection"; "Arith"]), "PA_preds_signature") nil;
+   tVar "D"; tVar "I";
+   tConstruct
+     {|
+     inductive_mind := (MPfile (["Reflection"; "Arith"]), "PA_funcs");
+     inductive_ind := 0 |} 1 nil;
+   tApp
+     (tConstruct
+        {|
+        inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t");
+        inductive_ind := 0 |} 1 nil)
+     ([tVar "D"; x;
+     tConstruct
+       {|
+       inductive_mind := (MPfile (["Datatypes"; "Init"; "Coq"]), "nat");
+       inductive_ind := 0 |} 0 nil;
+     tApp
+       (tConstruct
+          {|
+          inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t");
+          inductive_ind := 0 |} 0 nil) ([tVar "D"])])])).
+Notation quoteFalse := 
+(tInd
+   {|
+   inductive_mind := (MPfile (["Logic"; "Init"; "Coq"]), "False");
+   inductive_ind := 0 |} nil).
+Notation quoteAnd x y := 
+(tApp
+   (tInd
+      {|
+      inductive_mind := (MPfile (["Logic"; "Init"; "Coq"]), "and");
+      inductive_ind := 0 |} nil)
+   ([x;y])).
+Notation quoteForall x P := (tProd (nNamed x) (tVar "D") P).
+Notation quoteEq x y :=
+(tApp (tConst (MPfile (["Tarski"; "Arith"]), "i_P") nil)
+   ([tConst (MPfile (["Reflection"; "Arith"]), "PA_funcs_signature") nil; tConst (MPfile (["Reflection"; "Arith"]), "PA_preds_signature") nil;
+   tVar "D"; tVar "I"; tConstruct {| inductive_mind := (MPfile (["Reflection"; "Arith"]), "PA_preds"); inductive_ind := 0 |} 0 nil;
+   tApp (tConstruct {| inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t"); inductive_ind := 0 |} 1 nil)
+     ([tVar "D"; x;
+     tApp (tConstruct {| inductive_mind := (MPfile (["Datatypes"; "Init"; "Coq"]), "nat"); inductive_ind := 0 |} 1 nil)
+       ([tConstruct {| inductive_mind := (MPfile (["Datatypes"; "Init"; "Coq"]), "nat"); inductive_ind := 0 |} 0 nil]);
+     tApp (tConstruct {| inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t"); inductive_ind := 0 |} 1 nil)
+       ([tVar "D"; y; tConstruct {| inductive_mind := (MPfile (["Datatypes"; "Init"; "Coq"]), "nat"); inductive_ind := 0 |} 0 nil;
+       tApp (tConstruct {| inductive_mind := (MPfile (["VectorDef"; "Vectors"; "Coq"]), "t"); inductive_ind := 0 |} 0 nil) ([tVar "D"])])])])).
+Notation quoteLambda x P := (tLambda (nNamed x) (tVar "D") P).
+Definition failEnv (n:nat) : (TemplateMonad term):= tmFail ("Unbound " ++ string_of_nat n).
+Definition relLift (k : TemplateMonad term) : (TemplateMonad term) := kv <- k ;; match kv with var n => ret (var (S n)) | v => ret v end.
+Definition appendZero (k:TemplateMonad term) (v:nat -> TemplateMonad term) (n:nat) := match n with 0 => k | S n => v n end.
+Definition appendAndLift (k:TemplateMonad term) (v:nat -> TemplateMonad term) := appendZero k (v >> relLift).
+
+
+Fixpoint findTermRepresentation (t:Ast.term) (env:nat->TemplateMonad term) {struct t}: (TemplateMonad term) := let fail :=tmPrint t;;tmFail ("Cannot represent term") in match t with
+  quoteO => ret zero
+| quoteS v => 
+      st <- findTermRepresentation v env;;
+      ret (σ st)
+| tRel k => env k
+| _ => fail
+end.
+
+Fixpoint findPropRepresentation (t:Ast.term) (frees:nat) (env:nat->TemplateMonad term) {struct t}: (TemplateMonad (form)) := let fail :=tmPrint (frees,t);;tmFail ("Cannot represent form") in 
+  match (frees,t) with
+  (0,quoteFalse) => ret (fal)
+| (0,quoteAnd x y) => 
+      xr <- findPropRepresentation x 0 env;;
+      yr <- findPropRepresentation y 0 env;;
+      ret ((xr ∧ yr))
+| (0,quoteForall x P) =>
+      rv <- findPropRepresentation P 0 (appendAndLift (ret (var 0)) env);;
+      ret ((∀ rv))
+| (o,tProd _ P Q) =>
+      rP <- findPropRepresentation P 0 env;;
+      rQ <- findPropRepresentation Q 0 (appendZero (tmFail "Used var of implication precondition") env);;
+      ret (rP --> rQ)
+| (0,quoteEq tl tr) =>
+      tlr <- findTermRepresentation tl env;;
+      trr <- findTermRepresentation tr env;;
+      ret ((tlr == trr))
+| (S n,quoteLambda x P) => 
+      rv <- findPropRepresentation P n (appendAndLift (ret (var 0)) env);;
+      ret (rv)
+| _ => fail
+end.
+
+(*MetaCoq  Run (a <- tmQuote (iσ (iσ iO));; k <- findTermRepresentation a;; tmDefinition "res" k).*)
+(*MetaCoq  Run (a <- tmQuote (forall (x:D), x i= x);; k <- findPropRepresentation a failEnv;; tmPrint k).*)
+
+
+Ltac represent:= match goal with [ |- representableP ?n ?G ] =>
+  let fr := fresh "rep" in let kkk := fresh "sml" in let pr := fresh "denv" in let k y := (pose y as fr) in
+  (run_template_program (g <- (tmQuote G);;findPropRepresentation g n failEnv) k)
+  ;exists fr; pose (fun (_:nat) => iO) as pr; exists pr; split; easy
+  end.
+
+Goal (representableP 0 False).
+Proof. 
+represent.
+Qed.
+
+Goal (representableP 0 (False /\ False -> False)).
+Proof. 
+represent.
+Qed.
+
+Goal (representableP 0 (forall d, d i= iO)).
+Proof. 
+represent. 
+Qed.
+
+Goal (representableP 2 (fun (d e:D) => forall g, g i= d)).
+Proof. 
+represent.
+Qed.
+
+
+Print k.
+  Lemma sat_single (rho : nat -> D) (Phi : form) (t : term) :
+    (eval rho t .: rho) ⊨ Phi <-> rho ⊨ subst_form (t..) Phi.
+  Proof.
+    rewrite sat_comp. apply sat_ext. now intros [].
+  Qed.
+
+  (** Useful induction principle *)
+(* The following predicate expresses that a model satisfies the minimal axioms of PA i.e. all axioms except S x <> 0 *)
+  Definition sat_PA_minimal_axioms :=
+    forall rho,
+      rho ⊨ ax_succ_inj
+      /\ rho ⊨ ax_add_zero
+      /\ rho ⊨ ax_add_rec
+      /\ rho ⊨ ax_mult_zero
+      /\ rho ⊨ ax_mult_rec
+      /\ (forall phi, rho ⊨ (PA_induction phi) ).   
+  Variable AX : sat_PA_minimal_axioms.  
+  Theorem PAinduction_weak (phi : form) rho :
+    rho ⊨ phi[zero..] -> rho ⊨ (∀ phi --> phi[σ $ 0 .: S >> var]) -> rho ⊨ (∀ phi).
+  Proof.
+    destruct (AX rho) as (_&_&_&_&_&H). cbn. apply (H phi).
+  Qed.
+  Theorem PAinduction (P : D -> Prop) :
+    representable 1 P -> P iO -> (forall d, P d -> P (iσ d)) -> forall d, P d.
+    intros (phi & rho & repr) P0 IH. intros d. unfold represents in repr. rewrite repr.
+    apply PAinduction_weak.
+    - apply sat_single. apply repr. apply P0.
+    - cbn. intros d' H. apply repr, IH, repr in H.
+      apply sat_comp. eapply sat_ext; try apply H. now intros [].
+  Qed.
+
+  (** Examples *)
+
+  Lemma add_exists : forall (phi : form) rho, rho ⊨ phi -> exists sigma, sigma ⊨ (∃ phi).
+  Proof.
+    intros phi rho H; cbn. exists (fun n => match n with
+                           | O => rho 1
+                           | S m => rho (S n)
+                                end); exists (rho 0).
+    unfold scons. eapply sat_ext; try apply H. now intros [|[|]].
+  Qed.
+
+  Definition exist_times n (phi : form) := iter (fun psi => ∃ psi) n phi.
+  
+  Lemma add_n_exists : forall n,
+      forall (phi : form) rho, rho ⊨ phi -> exists sigma, sigma ⊨ (exist_times n phi).
+  Proof.
+    induction n; intros phi rho H.
+    - exists rho. auto. 
+    - destruct (IHn _ _ H) as [s Hs]. now refine (add_exists _ s _).
+  Qed.
+  
+  Lemma zero_or_succ : forall rho, rho ⊨ (∀ zero == $0 ∨ ∃ $1 == σ $0).
+  Proof.
+    intros rho. apply PAinduction_weak.
+    - left. now apply equality.
+    - intros d _. right. exists d; now apply equality.
+  Qed.
+
+
+  Goal forall d, iO = d \/ exists x, d = iσ x. 
+  Proof.
+    enough (forall rho, rho ⊨ (∀ zero == $0 ∨ ∃ $1 == σ $0)) as H. intros d.
+    specialize (H null). cbn in H. specialize (H d). revert H.
+    rewrite equality. cbn.
+    intros [<- | [x H]]. now left. right. rewrite equality in H. now exists x.
+    apply zero_or_succ.
+  Qed.
+
+  Goal forall d, iO = d \/ exists x, d = iσ x. 
+  Proof.
+    apply PAinduction.
+    pose (phi := zero == $0 ∨ ∃ $1 == σ $0).
+    - exists phi, (fun _ => iO). split.
+      + intros [<- | [x ->]].
+        * left. cbn. now rewrite equality.
+        * right. exists x. cbn. now rewrite equality.
+      + intros [H | [x H]].
+        * left. cbn in H. now rewrite equality in H.
+        * right. exists x. cbn in H. now rewrite equality in H.
+    - now left.
+    - intros d [<- |]; right. now exists iO. now exists d.
+  Qed.
+
+  Lemma add_rec_right :
+    forall d n, n i⊕ (iσ d) = iσ (n i⊕ d).
+  Proof.
+    intros n. apply PAinduction.
+    - pose (phi := ∀ $1 ⊕ σ $2 == σ ($1 ⊕ $2) ).
+      exists phi. exists (fun _ => n). intros d. cbn. split.
+      + intros H d0. rewrite equality. cbn. easy.
+      + intros H. specialize (H n). rewrite equality in H. cbn in H. auto.
+    - rewrite !add_zero; try reflexivity. all: firstorder.
+    - intros d IH. rewrite !add_rec. now rewrite IH. all: firstorder.
+  Qed.
+
+End Models.
